d042ef94ccab7043ae53be3052d23d6da038359d
WzSepia <wz45855283@163.com>
2021-08-23 09:15:39 +0800

    智慧树(科技树)组件 0.0.1v

---
+++ data.js
@@ -0,0 +1,71 @@
+/**
+ * 模拟数据
+ */
+var data = {
+	'leafs': [{
+		title: '省文旅厅',
+		text: '30张表（1%）',
+		titleStyle:`color:#f60;`,
+		textStyle:`color:#ff6600aa;`,
+		style: `border: 1px solid #f60;`,
+	}, {
+		title: '省文旅厅',
+		text: '30张表（2%）',
+		titleStyle:`color:#f60;`,
+		textStyle:`color:#ff6600aa;`,
+		style: `border: 1px solid #f60;`,
+	}, {
+		title: '省文旅厅',
+		text: '30张表（3%）',
+		titleStyle:`color:#e4ff65;`,
+		textStyle:`color:#e4ff65aa;`,
+		style: `border: 1px solid #e4ff65;`,
+	}, {
+		title: '省文旅厅',
+		text: '30张表（4%）',
+		titleStyle:`color:#00ffa8;`,
+		textStyle:`color:#00ffa8aa;`,
+		style: `border: 1px solid #00ffa8;`,
+	}, {
+		title: '省文旅厅',
+		text: '30张表（5%）',
+		titleStyle:`color:#4eaeff;`,
+		textStyle:`color:#4eaeffaa;`,
+		style: `border: 1px solid #4eaeff;`,
+	}, {
+		title: '省文旅厅',
+		text: '30张表（6%）',
+		titleStyle:`color:#4eaeff;`,
+		textStyle:`color:#4eaeffaa;`,
+		style: `border: 1px solid #4eaeff;`,
+	}],
+	roots: [{
+		title: '交通运输厅直属单位',
+		text: '1家',
+		style: `	background-color: #ff660033;
+							border: 1px solid #f60;
+							box-shadow: 0 0 20px 0px #f60;
+							color: #f60;`,
+	}, {
+		title: '交通运输厅直属单位',
+		text: '2家',
+		style: `  background-color: #e4ff6533;
+							border: 1px solid #e4ff65;
+							box-shadow: 0 0 20px 0px #e4ff65;
+							color: #e4ff65;`,
+	}, {
+		title: '交通运输厅直属单位',
+		text: '3家',
+		style: `  background-color: #00ffa833;
+							border: 1px solid #00ffa8;
+							box-shadow: 0 0 20px 0px #00ffa8;
+							color: #00ffa8;`,
+	}, {
+		title: '交通运输厅直属单位',
+		text: '4家',
+		style: `  background-color: #4eaeff33;
+							border: 1px solid #4eaeff;
+							box-shadow: 0 0 20px 0px #4eaeff;
+							color: #4eaeff;`,
+	}]
+};
---
+++ technologyTree.js
@@ -0,0 +1,89 @@
+/**
+ * 科技树组件
+ * @param {数据}  = [technologyTreeData] 
+ * @param {元素容器,id名}  = [technologyTree] 
+ */
+function technologyTree(data, eleRoot) {
+	let ele = document.querySelector(eleRoot);
+	if (!eleRoot || !ele) return false;
+	if (data.leafs.length < 6 || data.roots.length < 3) return false;
+	let boxHtml = `<div class="technologyTree">
+			<div class="body"></div>
+			<div class="foot"></div>
+		</div>`
+	ele.innerHTML = boxHtml;
+	let leafLength = data.leafs.length;
+	let rootLength = data.roots.length;
+	let locationLeaf = null; //叶子位置
+	let locationRoot = null; //跟位置
+	switch (leafLength) {
+		case 6:
+			locationLeaf = technologyTreeLeafs.six;
+			break;
+		case 7:
+			locationLeaf = technologyTreeLeafs.seven;
+			break;
+		case 8:
+			locationLeaf = technologyTreeLeafs.eight;
+			break;
+		case 9:
+			locationLeaf = technologyTreeLeafs.nine;
+			break;
+		case 10:
+			locationLeaf = technologyTreeLeafs.ten;
+			break;
+		case 11:
+			locationLeaf = technologyTreeLeafs.eleven;
+			break;
+		case 12:
+			locationLeaf = technologyTreeLeafs.twelve;
+			break;
+		default:
+			break;
+	}
+	switch (rootLength) {
+		case 3:
+			locationRoot = technologyTreeRoots.three;
+			break;
+		case 4:
+			locationRoot = technologyTreeRoots.four;
+			break;
+		case 5:
+			locationRoot = technologyTreeRoots.five;
+			break;
+		case 6:
+			locationRoot = technologyTreeRoots.six;
+			break;
+		default:
+			break;
+	}
+	let body = document.querySelector('.technologyTree .body');
+	let foot = document.querySelector('.technologyTree .foot');
+	let bodyHtml = '';
+	let footHtml = '';
+	data.leafs.forEach((leaf,index) => {
+		bodyHtml += `
+			<div style='left:${locationLeaf[index].x}px;top:${locationLeaf[index].y}px;${leaf.style};'>
+				<p title='${leaf.title}' style='${leaf.titleStyle}'>${leaf.title}</p>
+				<p title='${leaf.text}' style='${leaf.textStyle}'>${leaf.text}</p>
+			</div>
+		`
+	})
+	body.innerHTML = bodyHtml;
+	data.roots.forEach((root,index) => {
+		footHtml += `
+			<div style='left:${locationRoot[index].x}px;top:${locationRoot[index].y}px;${root.style};'>
+				<p title='${root.title}'>${root.title}</p>
+				<p title='${root.text}'>${root.text}</p>
+			</div>
+		`
+	})
+	foot.innerHTML = footHtml;
+	
+}
+
+/**
+ * @param {数据}  = [technologyTreeData] 
+ * @param {元素容器,id名}  = [technologyTree] 
+ */
+technologyTree(data, '.box');
